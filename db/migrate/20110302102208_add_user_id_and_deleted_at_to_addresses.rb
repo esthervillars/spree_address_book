26bf9bad42c5f9c0878af60ce468b88c38a7070d
Esther Villars <esther.villars@spiritclips.com>
2015-03-31 15:58:36 -0700

    fixes to master df3b7845f8a2383fb316d0c010908426c02231bf for 2-4-stable

--- db/migrate/20110302102208_add_user_id_and_deleted_at_to_addresses.rb
+++ db/migrate/20110302102208_add_user_id_and_deleted_at_to_addresses.rb
@@ -1,20 +1,20 @@
 class AddUserIdAndDeletedAtToAddresses < ActiveRecord::Migration
   def self.up
     change_table addresses_table_name do |t|
-      t.integer :user_id
-      t.datetime :deleted_at
+      t.integer :user_id, after: :id
+      t.datetime :deleted_at,  after: :created_at
     end
   end
 
   def self.down
     change_table addresses_table_name do |t|
       t.remove :deleted_at
-      t.remove :user_id    
+      t.remove :user_id
     end
   end
-  
+
   private
-  
+
   def self.addresses_table_name
     table_exists?('addresses') ? :addresses : :spree_addresses
   end
--- lib/generators/spree_address_book/install_generator.rb
+++ lib/generators/spree_address_book/install_generator.rb
@@ -1,14 +1,14 @@
 module SpreeAddressBook
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      class_option :auto_run_migrations, :type => :boolean, :default => false
+      class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require store/spree_address_book\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_address_book\n"
       end
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require store/spree_address_book\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/spree_address_book\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
@@ -16,14 +16,13 @@ module SpreeAddressBook
       end
 
       def run_migrations
-         res = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask "Would you like to run the migrations now? [Y/n]")
-         if res
-           run 'bundle exec rake db:migrate'
-         else
-           puts "Skiping rake db:migrate, don't forget to run it!"
-         end
+        res = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask "Would you like to run the migrations now? [Y/n]")
+        if res
+          run 'bundle exec rake db:migrate'
+        else
+          puts "Skiping rake db:migrate, don't forget to run it!"
+        end
       end
     end
   end
 end
-
